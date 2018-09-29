44bc91009850981bf729e280942f64c187b316c0
dfuwqe <wanzhichun@wirelessprv-10-194-54-223.near.illinois.edu>
2018-09-29 15:27:58 -0500

    visu

--- tree.py
+++ tree.py
@@ -11,13 +11,13 @@ class engine():
 	def __init__(self,filename):
 		#define parameters here
 		self.playSpeed = 1 
-		self.timeUnitBase = 5 #seconds
-		self.fps = 10
+		self.timeUnitBase = 1 #seconds
+		self.fps = 2
 		self.numLED = 60
 		self.animations=[self.animation1]
 		self.tableStartTime = datetime.timedelta(0,40) #day, seconds
 		# Comment serial port out if not connected to Arduino
-		self.serialPort = serial.Serial('/dev/cu.usbserial-DN02B86I', 250000)
+		# self.serialPort = serial.Serial('/dev/cu.usbserial-DN02B86I', 250000)
 
 		#variables
 		self.currentTime = 0
@@ -28,7 +28,7 @@ class engine():
 		self.ASmap = dict() #dictionary -- key: AS number, val: LED number
 		self.maxTime = 0 
 		self.frameInterval = 1/(self.fps)
-		self.frames = [(["1"]* self.numLED+["\n"]) for i in range(self.timeUnit*self.fps)]
+		self.frames = [([0]* self.numLED) for i in range(self.timeUnit*self.fps)]
 		self.timer = 0
 
 		self.set_up_step(filename)
@@ -45,12 +45,14 @@ class engine():
 		#this step is to process the table to perform:
 		#AS - LED position matching
 		ASlist = self.table["AS"].value_counts().index.tolist()
+		print(ASlist)
 		if(len(ASlist) >= self.numLED):
 			ASlist = ASlist[:self.numLED]
 		else:
 			ASlist = ASlist + [0] * (self.numLED - len(ASlist))
 
 		shuffle(ASlist)
+		
 		#Map AS number to LED number
 		for i in range(self.numLED):
 			self.ASmap[ASlist[i]] = i
@@ -88,9 +90,9 @@ class engine():
 			
 
 	def extract_data_for_one_time_unit(self):
+		print(self.currentTime)
 		startTime = self.currentTime
 		self.currentTime += datetime.timedelta(seconds=self.timeUnit)
-		print(self.currentTime)
 		endTime = self.currentTime
 		return self.table[(self.table["time"]>= startTime) & (self.table["time"] < endTime)]
 
@@ -101,7 +103,8 @@ class engine():
 	def draw_frame(self,index):
 		#Note: since it's singled-threaded right now. I'll just let the thread wait until the next frame after it finishes processing
 		strList = self.frames[index]
-		self.write_to_serial("".join(strList))
+		strList = [chr(x) for x in strList]
+		self.write_to_serial("".join(strList)+"\n")
 		timeElapsed = time.time() - self.timer
 		self.wait_for_one_full_frame(timeElapsed)
 		self.timer = time.time()
@@ -116,22 +119,25 @@ class engine():
 		#middle-ware
 		self.serialPort.write(frame.encode())
 		print(frame)
-		# pass
 		# print(self.serialPort.readline())
 			
 
 
+	#control 0 - 255. 
+	#Visualization no.2
 	def animation1(self,data):
 		#ASes that have updates
-		ASlist = data["AS"].value_counts().index.tolist()
+		ASlist = data["AS"].unique().tolist()
 		#blink at 2Hz
 		for x in ASlist:
-			led = self.ASmap[x]
-			for i in range(len(self.frames)):
-				if (i%10)<5:
-					self.frames[i][led] = "1"
-				else:
-					self.frames[i][led] = '0'
+			if x in self.ASmap:
+				led = self.ASmap[x]
+				self.frames[0][led] = 0	#blink
+				#check if this is annouce
+				test = data[(data["AS"]==x) & (data["action"] == "A")]
+				if not test.empty:
+					self.frames[1][led] = int(test["content"].iloc[-1].split('.')[0])
+
 
 
 
@@ -141,7 +147,7 @@ class engine():
 
 def main():
 	e = engine("output.txt")
-	e.show()
+	# e.show()
 
 
 if __name__ == '__main__':
